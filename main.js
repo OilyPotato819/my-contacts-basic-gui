34d30e7270a9bbf34c97bf377494cce00d6dab0e
Oliver Schneider <o.schneider@share.epsb.ca>
2023-03-16 12:19:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,62 @@
 // My Contacts Basic
 
 // HTML Elements
-let goBtnEl = document.getElementById('go-btn');
-let menuEl = document.getElementById('menu');
-let outputEl = document.getElementById('output');
+let goBtnEl = document.getElementById("go-btn");
+let menuEl = document.getElementById("menu");
+let outputEl = document.getElementById("output");
 
 // Go Btn - Menu Listener
-goBtnEl.addEventListener('click', goBtnHandler);
+goBtnEl.addEventListener("click", goBtnHandler);
 
 function goBtnHandler() {
   // Get Menu Selection
   let selection = menuEl.value;
 
-  if (selection === 'display-all') {
+  if (selection === "display-all") {
     displayContacts();
-  } else if (selection === 'add') {
+  } else if (selection === "add") {
     addContact();
-  } else if (selection === 'remove') {
+  } else if (selection === "remove") {
     removeContact();
-  } else if (selection === 'display-name') {
+  } else if (selection === "display-name") {
     displayByName();
-  } else if (selection === 'display-country') {
+  } else if (selection === "display-country") {
     displayByCountry();
   }
 }
 
 // MENU FUNCTIONS
 function displayContacts() {
-  console.log('Display Contacts');
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  console.log(tasks);
+
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+
+    let string = `<div>
+    <h1>${task.index + 1}: ${task.name}</h1>
+    <p>
+    ${task.email}\n
+    ${task.phoneNumber} (${task.country})
+    </p>
+    </div>`;
+
+    outputEl.append(string);
+  }
 }
 
 function addContact() {
-  console.log('Add Contact');
+  console.log("Add Contact");
 }
 
 function removeContact() {
-  console.log('Remove Contact');
+  console.log("Remove Contact");
 }
 
 function displayByName() {
-  console.log('Display by Name');
+  console.log("Display by Name");
 }
 
 function displayByCountry() {
-  console.log('Display by Country');
+  console.log("Display by Country");
 }
